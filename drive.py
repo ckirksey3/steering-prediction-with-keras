40b15b52e92a5dcb333ba560b08b27de5050c5df
Caleb Kirksey <ckirksey3@gmail.com>
2016-12-14 09:00:46 -0800

    get model to train successfully on a few sample images

--- drive.py
+++ drive.py
@@ -14,6 +14,7 @@ from io import BytesIO
 
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+from model import process_img
 
 
 sio = socketio.Server()
@@ -33,7 +34,7 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
-    transformed_image_array = image_array[None, :, :, :]
+    transformed_image_array = process_img(image_array)
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
--- model.py
+++ model.py
@@ -4,19 +4,18 @@ from keras.utils import np_utils
 from keras.layers import Conv2D, Flatten, Reshape
 from keras.models import model_from_json
 import json
-
-# Normalization
-# X_train = X_train.astype('float32')
-# X_val = X_val.astype('float32')
-# X_train = X_train / 255 - 0.5
-# X_val = X_val / 255 - 0.5
+from PIL import Image
+import io, base64
+import numpy as np
+import scipy.misc
+from matplotlib import pyplot as plt
 
 #Create CNN based on NVIDIA paper (http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf)
 model = Sequential()
 # model.add(Reshape((66, 200, 3), input_shape=(160, 320, 3)))
 #Deviates from NVIDIA paper
 # model.add(Conv2D(3, 5, 5, input_shape=(66, 200, 3), subsample=(2, 2), activation='relu'))
-model.add(Conv2D(3, 5, 5, input_shape=(160, 320, 3), subsample=(2, 2), activation='relu'))
+model.add(Conv2D(3, 5, 5, input_shape=(80, 160, 3), subsample=(2, 2), activation='relu'))
 model.add(Conv2D(24, 5, 5, input_shape=(31, 98, 3), subsample=(2, 2), activation='relu'))
 model.add(Conv2D(36, 5, 5, input_shape=(5, 22, 3), subsample=(2, 2), activation='relu'))
 model.add(Conv2D(48, 3, 3, input_shape=(3, 20, 3), activation='relu'))
@@ -28,7 +27,7 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(1, activation='softmax'))
 
 model.summary()
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['accuracy'])
 
@@ -38,31 +37,36 @@ with open('model.json','w') as f:
         json.dump(json_string,f)
 
 #Train model
-imgString = data["image"]
-image = Image.open(BytesIO(base64.b64decode(imgString)))
-image_array = np.asarray(image)
-transformed_image_array = image_array[None, :, :, :]
-# history = model.fit(X_train_flat, Y_train,
-#                     batch_size=128, nb_epoch=5,
-#                     verbose=1, validation_data=(X_val_flat, Y_val))
-
 def process_line(line):
-    
+    items = [x.strip() for x in line.split(',')]
+    center_img_loc = items[0]
+    image = Image.open(center_img_loc)
+    image_array = np.asarray(image)
+    transformed_image_array = process_img(image_array)
+    return transformed_image_array, np.array([items[3]])
+
+def process_img(image_array):
+    image_array = scipy.misc.imresize(image_array, (80, 160))
+    return image_array[None, :, :, :]
 
 def generate_arrays_from_file(path):
     while 1:
-    f = open(path)
-    for line in f:
-        # create numpy arrays of input data
-        # and labels, from each line in the file
-        x1, x2, y = process_line(line)
-        yield ({'input_1': x1, 'input_2': x2}, {'output': y})
-    f.close()
-
+        f = open(path)
+        for line in f:
+        # for i in range(5):
+        #     line = f.readline()
+            # create numpy arrays of input data
+            # and labels, from each line in the file
+            center_img, steering_angle = process_line(line)
+            # print(center_img.shape, steering_angle)
+            # plt.imshow(center_img.squeeze(), interpolation='nearest')
+            # plt.show()
+            yield (center_img, steering_angle)
+        f.close()
+# generate_arrays_from_file('test_driving_log.csv')
 model.fit_generator(generate_arrays_from_file('driving_log.csv'),
-        samples_per_epoch=10000, nb_epoch=10)
+        samples_per_epoch=1000, nb_epoch=5)
 
 model.save_weights("model.h5")
 #TODO implement transfer learning from previous successful models
 #TODO get at least 40k samples (currently 1.3K)
-#TODO Re-size the input image down by 2
